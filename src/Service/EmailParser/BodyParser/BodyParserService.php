0d255647cbdb702f657f2cd6410aeb335579b61c
Milos Popovic <milos.pop.004@gmail.com>
2025-08-18 09:57:06 +0200

    Implement twig sandbox for security and input html sanitization

--- src/Service/EmailParser/BodyParser/BodyParserService.php
+++ src/Service/EmailParser/BodyParser/BodyParserService.php
@@ -2,20 +2,34 @@
 
 namespace App\Service\EmailParser\BodyParser;
 
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
+
 class BodyParserService
 {
     private iterable $parsers;
+    private HtmlSanitizer $sanitizer;
 
     public function __construct(iterable $parsers)
     {
         $this->parsers = $parsers;
+        $this->sanitizer = new HtmlSanitizer(
+            (new HtmlSanitizerConfig())->allowSafeElements()
+        );
+    }
+
+    private function sanitizeTemplate(string $template): string
+    {
+        return $this->sanitizer->sanitize($template);
     }
 
     public function parse(string $templateContent, string $extension, array $variables = []): string
     {
         foreach ($this->parsers as $parser) {
             if ($parser->supports($extension)) {
-                return $parser->parseTemplate($templateContent, $variables);
+                $parsedTemplate = $parser->parseTemplate($templateContent, $variables);
+                $sanitizedTemplate = $this->sanitizer->sanitize($parsedTemplate);
+                return $sanitizedTemplate;
             }
         }
 
--- src/Service/EmailParser/BodyParser/TwigBodyParserService.php
+++ src/Service/EmailParser/BodyParser/TwigBodyParserService.php
@@ -5,26 +5,57 @@ namespace App\Service\EmailParser\BodyParser;
 use App\Service\EmailParser\BodyParser\BodyParserInterface;
 use App\Service\EmailParser\BodyParser\MjmlBodyParserService;
 use App\Service\EmailParser\TwigContextBuilderService;
+use Symfony\Bundle\TwigBundle\DependencyInjection\TwigExtension;
 use Twig\Environment;
 use Twig\Error\SyntaxError;
+use Twig\Extension\SandboxExtension;
+use Twig\Loader\ArrayLoader;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityPolicy;
 
 class TwigBodyParserService implements BodyParserInterface
 {
+    private Environment $twigSandbox;
+
     public function __construct(
-        private Environment $twig,
         private TwigContextBuilderService $twigContextBuilderService,
         private MjmlBodyParserService $mjmlBodyParserService,
-    ) {}
+    ) {
+        $this->twigSandbox = $this->createSandbox();
+    }
 
     public function supports(string $extension): bool
     {
         return str_contains($extension, 'twig');
     }
 
+    private function createSandbox(): Environment
+    {
+        $allowedTags = ['if', 'for', 'set'];
+        $allowedFilters = ['escape', 'upper', 'lower', 'length', 'format_datetime', 'date', 'round'];
+        $allowedFunctions = [];
+        $allowedMethods = [];
+        $allowedProperties = [];
+
+        $policy = new SecurityPolicy(
+            $allowedTags,
+            $allowedFilters,
+            $allowedFunctions,
+            $allowedMethods,
+            $allowedProperties
+        );
+
+        $loader = new ArrayLoader();
+        $twigSandbox = new Environment($loader);
+        $twigSandbox->addExtension(new SandboxExtension($policy, true));
+
+        return $twigSandbox;
+    }
+
     public function parseTemplate(string $templateContent, array $variables = []): string
     {
         try {
-            $template = $this->twig->createTemplate($templateContent);
+            $template = $this->twigSandbox->createTemplate($templateContent);
             $context = $this->twigContextBuilderService->getContext($variables);
 
             $content = $template->render($context);
@@ -35,6 +66,9 @@ class TwigBodyParserService implements BodyParserInterface
             return $content;
         }
         catch (SyntaxError $e) {
+            throw new ParserException($e->getMessage(), 400);
+        }
+        catch (SecurityError $e) {
             throw new ParserException($e->getMessage(), 400);
         }
     }
