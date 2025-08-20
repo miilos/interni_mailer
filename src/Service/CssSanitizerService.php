470fc77c033f0e0466c454045a2b19ed93559eeb
Milos Popovic <milos.pop.004@gmail.com>
2025-08-20 18:17:45 +0200

    Fix css sanitization

--- src/Service/CssSanitizerService.php
+++ src/Service/CssSanitizerService.php
@@ -2,7 +2,50 @@
 
 namespace App\Service;
 
+use Sabberworm\CSS\Parser;
+
 class CssSanitizerService
 {
+    private array $allowedProperties;
 
+    public function __construct(array $allowedProperties)
+    {
+        $this->allowedProperties = $allowedProperties;
+    }
+
+    public function sanitize(string $html): string
+    {
+        // find all style tags and replace them
+        return preg_replace_callback(
+            '/<style\b[^>]*>(.*?)<\/style>/is',
+            function ($matches) {
+                $cleanCss = $this->sanitizeCss($matches[1]);
+                return "<style>{$cleanCss}</style>";
+            },
+            $html
+        );
+    }
+
+    public function sanitizeCss(string $css): string
+    {
+        $parser = new Parser($css);
+        try {
+            $cssDocument = $parser->parse();
+        } catch (\Exception $e) {
+            // remove the style tag if there's any malformed css
+            return '';
+        }
+
+        foreach ($cssDocument->getAllDeclarationBlocks() as $block) {
+            foreach ($block->getRules() as $rule) {
+                $property = $rule->getRule();
+
+                if (!in_array($property, $this->allowedProperties)) {
+                    $block->removeRule($rule);
+                }
+            }
+        }
+
+        return $cssDocument->render();
+    }
 }
--- src/Service/EmailParser/BodyParser/BodyParserService.php
+++ src/Service/EmailParser/BodyParser/BodyParserService.php
@@ -2,17 +2,20 @@
 
 namespace App\Service\EmailParser\BodyParser;
 
+use App\Service\CssSanitizerService;
 use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
 
 class BodyParserService
 {
     private iterable $parsers;
     private HtmlSanitizerInterface $sanitizer;
+    private CssSanitizerService $cssSanitizer;
 
-    public function __construct(iterable $parsers, HtmlSanitizerInterface $sanitizer)
+    public function __construct(iterable $parsers, HtmlSanitizerInterface $sanitizer, CssSanitizerService $cssSanitizer)
     {
         $this->parsers = $parsers;
         $this->sanitizer = $sanitizer;
+        $this->cssSanitizer = $cssSanitizer;
     }
 
     private function sanitizeTemplate(string $template): string
@@ -24,8 +27,24 @@ class BodyParserService
     {
         foreach ($this->parsers as $parser) {
             if ($parser->supports($extension)) {
+                // parse the template
                 $parsedTemplate = $parser->parseTemplate($templateContent, $variables);
+
+                // extract the <head> if it exists because the Symfony Sanitizer discards it
+                preg_match('#<head.*?>(.*?)</head>#is', $parsedTemplate, $matches);
+                $head = $matches[0] ?? '';
+
+                // sanitize any <style> tags in the <head>
+                // this is done without Symfony Sanitizer because it removes <head> and <style> tags
+                $sanitizedHead = $this->cssSanitizer->sanitize($head);
+
+                // sanitize the rest of the template
                 $sanitizedTemplate = $this->sanitizeTemplate($parsedTemplate);
+
+                // find where the <body> tag starts and insert the head right before it
+                $bodyStart = strpos($sanitizedTemplate, '<body');
+                $sanitizedTemplate = substr_replace($sanitizedTemplate, $sanitizedHead, $bodyStart, 0);
+
                 return $sanitizedTemplate;
             }
         }
